2bcc0901614d202bb252dcf2e053bc8bce284a63
idesignsng <79612610+idesignsng@users.noreply.github.com>
2021-12-24 22:42:59 +0100

    Update App.js

--- app/components/App.js
+++ app/components/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
             <Router>
                 <div>
                     <NavBar />
-                    <Route exact path="/QuickBill/" component={HomePage} />
+                    <Route exact path="/" component={HomePage} />
                     <Route path="/QuickBill/dashboard" component={Dashboard} />
                     <Route path="/QuickBill/preview" component={Preview}></Route>
                 </div>
